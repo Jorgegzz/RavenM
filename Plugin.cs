9f7ab77d97969f861c9c027b6b7450442fbe9ab4
Chryses <62386060+veroiuxl@users.noreply.github.com>
2022-08-14 22:52:16 +0200

    Added expandable Custom Options (#10)
    * Added Custom Options for rebinding and enabling features
    
    * Fixed formatting in OptionsPatch.cs
    
    * Implemented Toggle to enable or disable RavenMHUD
    
    * Delete RavenM.csproj
    
    * Delete RavenM.sln
    
    * Brought the csproj back to life
    
    * Update RavenM.csproj

--- Plugin.cs
+++ Plugin.cs
@@ -120,7 +120,6 @@ namespace RavenM
                 return;
 
             SteamAPI.RunCallbacks();
-
             if (!FirstSteamworksInit)
             {
                 FirstSteamworksInit = true;
