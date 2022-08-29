73df1e015970461583df78ee57a09d7d7efcfc44
Jorgegzz <A01571510@tec.mx>
2022-08-28 20:21:22 -0500

    Unfinished Rich Presence Buttons In-Game

--- DiscordIntegration.cs
+++ DiscordIntegration.cs
@@ -15,8 +15,9 @@ namespace RavenM
         public long discordClientID = 1007054793220571247;
 
         public long startSessionTime;
-        
-        
+
+        private LobbyManager _lobbyManager;
+        private ActivityManager _activityManager;
         private void Start()
         {
             Discord = new Discord(discordClientID, (UInt64) CreateFlags.Default);
@@ -24,6 +25,9 @@ namespace RavenM
             startSessionTime = ((DateTimeOffset) DateTime.Now).ToUnixTimeSeconds();
 
             StartCoroutine(StartActivities());
+            
+            _lobbyManager = Discord.GetLobbyManager();
+            _activityManager = Discord.GetActivityManager();
         }
         
         IEnumerator StartActivities()
@@ -32,9 +36,13 @@ namespace RavenM
             yield return new WaitUntil(GameManager.IsInMainMenu);
             UpdateActivity(Discord, Activities.InMenu);
         }
-
+        
+        // Private Variables that makes me question my coding skills
         private float _timer;
         private string _gameMode = "Insert Game Mode";
+        private LobbyTransaction txn;
+        private Lobby _currentLobby;
+        private bool _isDiscordLobbyActive;
         private void FixedUpdate()
         {
            Discord.RunCallbacks();
@@ -44,23 +52,46 @@ namespace RavenM
            if (_timer > 5f)
            {
                if (GameManager.instance == null) { return; }
-
-               if (LobbySystem.instance.InLobby && !GameManager.IsIngame())
+               
+               
+               if (!LobbySystem.instance.InLobby && GameManager.IsIngame()) // Playing SinglePlayer
                {
                    var dropdown = InstantActionMaps.instance.gameModeDropdown;
                    _gameMode = dropdown.options[dropdown.value].text;
-                   int currentLobbyMembers = SteamMatchmaking.GetNumLobbyMembers(LobbySystem.instance.ActualLobbyID);
-                   int currentLobbyMemberCap = SteamMatchmaking.GetLobbyMemberLimit(LobbySystem.instance.ActualLobbyID);
-                   UpdateActivity(Discord, Activities.InLobby, _gameMode ,currentLobbyMembers, currentLobbyMemberCap);
+                   UpdateActivity(Discord, Activities.InSinglePlayerGame, _gameMode);
+               }
+               else if (LobbySystem.instance.InLobby)
+               {
+                   if (!_isDiscordLobbyActive)
+                   {
+                       txn = _lobbyManager.GetLobbyCreateTransaction();
+                       txn.SetCapacity(16);
+                       txn.SetType(LobbyType.Public); // TODO: Detect if the lobby is browsable or not to determine the LobbyType will be public or private
+                   
+                       _lobbyManager.CreateLobby(txn, (Result result, ref Lobby lobby) =>
+                       {
+                           _currentLobby = lobby;
+                           Plugin.logger.LogInfo($"lobby {lobby.Id} created with secret {lobby.Secret}");
+                       });
+                       _isDiscordLobbyActive = true; // TODO: For now, there is no way the discord lobby destroys itself once you are out of a lobby
+                   }
+                   
+                   if (!GameManager.IsIngame()) // Waiting In lobby Multiplayer
+                   {
+                       var dropdown = InstantActionMaps.instance.gameModeDropdown;
+                       _gameMode = dropdown.options[dropdown.value].text;
+                       int currentLobbyMembers = SteamMatchmaking.GetNumLobbyMembers(LobbySystem.instance.ActualLobbyID);
+                       int currentLobbyMemberCap = SteamMatchmaking.GetLobbyMemberLimit(LobbySystem.instance.ActualLobbyID);
+                       UpdateActivity(Discord, Activities.InLobby, _gameMode ,currentLobbyMembers, currentLobbyMemberCap);
+                   }
+                   else // Playing Multiplayer
+                   {
+                       int currentLobbyMembers = SteamMatchmaking.GetNumLobbyMembers(LobbySystem.instance.ActualLobbyID);
+                       int currentLobbyMemberCap = SteamMatchmaking.GetLobbyMemberLimit(LobbySystem.instance.ActualLobbyID);
+                       UpdateActivity(Discord, Activities.InMultiPlayerGame, _gameMode ,currentLobbyMembers, currentLobbyMemberCap);
+                   }
                }
 
-               if (GameManager.IsIngame())
-               {
-                   int currentLobbyMembers = SteamMatchmaking.GetNumLobbyMembers(LobbySystem.instance.ActualLobbyID);
-                   int currentLobbyMemberCap = SteamMatchmaking.GetLobbyMemberLimit(LobbySystem.instance.ActualLobbyID);
-                   UpdateActivity(Discord, Activities.InMultiPlayerGame, _gameMode ,currentLobbyMembers, currentLobbyMemberCap);
-               }
-               
                _timer = 0f;
            }
         }
@@ -111,10 +142,20 @@ namespace RavenM
                             LargeText = "RavenM",
                         },
                         Party = {
+                            Id = _currentLobby.Id.ToString(),
                             Size = {
                                 CurrentSize = currentPlayers,
                                 MaxSize = maxPlayers,
                             },
+                        },
+                        Secrets =
+                        {
+                            Match = "foo match secret", // TODO: I don't know how the discord secrets work for now,
+                                                        // TODO: I just know that if I assign them a string, the invite /ask to join buttons appear
+                                                        
+                                                        // TODO: This Invite/Ask to Join Thing Should also be working in the MultiPlayerGame Activity-
+                                                        // TODO: once I get this thing working in the Lobby Activity
+                            Join= "abcd",
                         },
                         Instance = true,
                     };
